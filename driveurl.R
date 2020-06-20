5f72b54fb489a9148b1068ffa695464d0abd0d5e
crissthiandi <albertocenaa@gmail.com>
2020-06-19 19:18:01 -0500

    Se agrego comentarios para ayudar con la lectura del codigo

--- driveurl.R
+++ driveurl.R
@@ -1,5 +1,7 @@
 #extracción de id
 
+#función que extrae el id de un link generico de google drive,
+#se probo en links obtenidos de compartir con todo publico con el link
 getidgoogle= function(link) {
   a=regexpr("/d/",link)
   a=as.integer(a)
@@ -10,9 +12,15 @@ getidgoogle= function(link) {
 }
 
 #leer desde internet
+#esta función lee un link, usa la función getidgoogle() para extraer
+#la id para su descarga
+
 getcsv_drive= function(link){
   id=getidgoogle(link = link)
+  #descarga el objeto con ayuda de un link de desgarga que 
+  #requiere de la id del documentos, usando sprintf()
   a=read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+  #regresa el objeto descargado
   return(a)
 }
 
