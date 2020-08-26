f40be804e6b2cac3b0091aec73895c549183e0b3
crissthiandi <albertocenaa@gmail.com>
2020-08-26 15:36:14 -0500

    Alto, parece que todos estos intentos estan descontinuados para la versión de ggplot que se esta trabajando

--- edicion_on_grid.R
+++ edicion_on_grid.R
@@ -3,6 +3,10 @@ d <- ggplot(mtcars, aes(x=gear)) +
   geom_bar(aes(y=gear), stat="identity", position="dodge") +
   facet_wrap(~cyl)
 
+geditGrob <- function(..., grep = TRUE, global = TRUE) {	
+  editGrob(..., grep = grep, global = global)	
+}
+
 grob <- ggplotGrob(d)
 strip_bg <- grid.ls(getGrob(grob, "strip.background.rect",
                             grep=TRUE, global=TRUE))$name
@@ -10,7 +14,26 @@ panel_bg <- grid.ls(getGrob(grob, "panel.background.rect",
                             grep=TRUE, global=TRUE))$name
 strip_text <- grid.ls(getGrob(grob, "strip.text.x",
                               grep=TRUE, global=TRUE))$name
-grob <- geditGrob(grob, strip_bg[2], gp=gpar(fill="gray60"))
-grob <- geditGrob(grob, panel_bg[2], gp=gpar(fill="darkolivegreen2"))
-grob <- geditGrob(grob, strip_text[2], gp=gpar(col="white"))
+grob <- geditGrob(grob, strip_bg[2], gp=gpar(fill="gray60"),grep=T,global = T)
+grob <- geditGrob(grob, panel_bg[2], gp=gpar(fill="darkolivegreen2"),grep=T,global = T)
+grob <- geditGrob(grob=grob, strip_text[2], gp=gpar(col="white"),grep=T,global = T)
+grid.draw(grob)
+
+
+############ Intento 2 ----
+
+grob <- ggplotGrob(d)
+
+elem <- grob$grobs$panel2
+panel_bg <- grid.ls(getGrob(elem, "panel.background.rect", grep=TRUE))$name
+grob$grobs$panel2 <- editGrob(elem, panel_bg, gp=gpar(fill="darkolivegreen"), grep=TRUE)
+
+elem <- grob$grobs$strip_t.1
+strip_bg <- grid.ls(getGrob(elem, "strip.background.rect", grep=TRUE))$name
+grob$grobs$strip_t.1 <- editGrob(elem, strip_bg, gp=gpar(fill="gray60"), grep=TRUE)
+
+elem <- grob$grobs$strip_t.1
+strip_text <- grid.ls(getGrob(elem, "strip.text.x.text", grep=TRUE))$name
+grob$grobs$strip_t.1 <- editGrob(elem, strip_text, gp=gpar(col="white"), grep=TRUE)
+
 grid.draw(grob)
