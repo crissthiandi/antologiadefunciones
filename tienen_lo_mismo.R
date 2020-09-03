e053c8576b1299ac1768095f08ecbcc46cdd23d9
crissthiandi <albertocenaa@gmail.com>
2020-09-03 17:45:57 -0500

    Alto, Versión1.0 Se reconocen los elementos en comun si los hay Se reconocen los elementos de cada data frame Se imprime una lista Integración con la función merge() si se gusta Se ingresa parametro de ayuda para merge

--- tienen_lo_mismo.R
+++ tienen_lo_mismo.R
@@ -2,6 +2,53 @@
 
 
 
-tienen_lo_mismo = function(base, base_2) {
+tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,...) {
+  if(ayuda){browseURL("https://rpubs.com/Rortizdu/140174")}
+  #parche para inconcistencias
+  if(!por_merge){por_join=T}
+  #se checan los nombres
+  nombre=names(base_x)
+  if(length(nombre)==1){
+    names(base_x)="base_col"
+  }else{
+    names(base_x)[1]="base_col"
+  }
   
+  nombre=names(base_y)
+  if(length(nombre)==1){
+    names(base_y)="base_col"
+  }else{
+    names(base_y)[1]="base_col"
+  }
+  #se checa join
+  
+  if(por_join){
+    base_output=as.list(NULL)
+    base_output[["en_comun"]]=base_x %>% inner_join(base_y ,by="base_col")
+    base_output[["solo_en_x"]]=base_x %>% left_join(base_y ,by="base_col")
+    base_output[["solo_en_y"]]=base_x %>% right_join(base_y ,by="base_col")
+    base_output[["Excluidos"]]=base_x %>% filter(base_output$en_comun)
+  }else{
+    base_output=merge(base_x,base_y,by = "base_col",...)
+  }
+  
+  return(base_output)
 }
+
+#testing
+
+a=tibble(
+  x=1:5
+)
+
+b=tibble(
+  y=2:8
+)
+
+tienen_lo_mismo(a,b,all.y=T)
+
+
+
+
+
+
