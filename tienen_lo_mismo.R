5a02b0911417ef6096dda14e27acc88e5a8c3e71
crissthiandi <albertocenaa@gmail.com>
2020-09-03 23:38:45 -0500

    Muy alto, Parametro FULL & Recursividad Se agrego Ayuda extra con un link a usos de merge extra, ahora son 2 Se Crea la opción FULL la cual al ser True te permite calcular los miembros diferentes para cada base La forma de hacer funcionar full hace uso de recurividad en funciones, aunque es funcional se tiene el pentidente de una posible recalculo de elementos Lo anterior podria consumir el doble de recursos de los esperados para el calculo de Excluidos_y

--- tienen_lo_mismo.R
+++ tienen_lo_mismo.R
@@ -2,8 +2,11 @@
 
 
 
-tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,...) {
-  if(ayuda){browseURL("https://rpubs.com/Rortizdu/140174")}
+tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,FULL=F,...) {
+  if(ayuda){
+    browseURL("https://rpubs.com/Rortizdu/140174")
+    browseURL("https://pmoracho.github.io/blog/2017/06/26/Combinacion-de-datos-en-R/")
+  }
   #parche para inconcistencias
   if(!por_merge){por_join=T}
 
@@ -29,16 +32,27 @@ tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,...) {
   if(por_join){
     base_output=as.list(NULL)
     base_output[["en_comun"]]=base_x %>% inner_join(base_y ,by="base_col") %>% select(base_col)
-    base_output[["solo_en_x"]]=base_x %>% left_join(base_y ,by="base_col")
-    base_output[["solo_en_y"]]=base_x %>% right_join(base_y ,by="base_col")
-    base_output[["Excluidos"]]=base_x %>% right_join(base_y ,by="base_col") %>%filter(is.na(Originales_x))
-    message("Excluido incluye los elementos que no estan en x pero si en y")
+    base_output[["todo_en_x"]]=base_x %>% left_join(base_y ,by="base_col")
+    base_output[["todo_en_y"]]=base_x %>% right_join(base_y ,by="base_col")
+    base_output[["Excluidos_y"]]=base_x %>% right_join(base_y ,by="base_col") %>%filter(is.na(Originales_x))
+    message("Excluido_y incluye los elementos que no estan en x pero si en y")
   }else{
     message("Use all=T para obtener los elementos que no estan en x pero si en y")
     base_output=merge(base_x,base_y,by = "base_col",...)
   }
   
+  if(FULL){
+    base_y["Originales_x"]=base_y["base_col"]
+    base_x["Originales_x"]=NULL
+    
+    #Resolver luego posible generador de latencia uwu reparar luego
+    base_output[["Excluidos_x"]]=tienen_lo_mismo(base_x = base_y,base_y = base_x,FULL=F,por_merge = F)$Excluidos_y
+    message("Excluido_x incluye los elementos que no estan en y pero si en x")
+  }
+  
+
   return(base_output)
+  
 }
 
 #testing ----
@@ -51,8 +65,8 @@ tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,...) {
 #   y=2:8
 # )
 # 
-# tienen_lo_mismo(a,b,por_merge = T,all=T)
-
+# tienen_lo_mismo(a,b,por_merge = F,FULL = T)
+# 
 
 
 
