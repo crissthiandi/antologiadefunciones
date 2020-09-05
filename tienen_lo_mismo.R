5ffc832cae2a5fb65f6875120f5e536ddec703f4
crissthiandi <albertocenaa@gmail.com>
2020-09-05 15:29:57 -0500

    Muy alto, Recusividad Soporte & Fixed rendimiento Se crean los parametros y se hacen las modificaciones para soportar hacer recursividad y de esta forma soportar que la función haga los calculos de los excluidos en x y exclusivos de x sin repetir calculos que en bases enormes pueden alentar de forma sensitiva el proceso. Se comentan los usos de nueva logica con el fin de ayudar en la lectura de codigo Se testea el codigo y todo parece ir bien Doble condicional en ciertos if con el fin de dar soporte a la recursión ACTUALIZACIÓN: La nueva forma de trabajar de tienen_lo_mismo() te hace el calculo de los elementos en comun y las diferencias de la base_x con respecto a la base_y de forma estandar, solo en versión full te hace dicho calculo con excluciones totales. No compatible FULL=T con metodo merge, por lo que no hara el proceso. Se debe agregar aviso para uso de FULL pero como es de uso personal se omite dicho aviso.

--- tienen_lo_mismo.R
+++ tienen_lo_mismo.R
@@ -1,8 +1,8 @@
-#función que busca ver si dos columnas tiene los mismos elementos sin importar el roden
+#función que busca ver si dos columnas tiene los mismos elementos sin importar el orden
 
 
 
-tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,FULL=F,...) {
+tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,FULL=F,recursion=T,...) {
   if(ayuda){
     browseURL("https://rpubs.com/Rortizdu/140174")
     browseURL("https://pmoracho.github.io/blog/2017/06/26/Combinacion-de-datos-en-R/")
@@ -27,27 +27,34 @@ tienen_lo_mismo = function(base_x, base_y,por_join=F,por_merge=T,ayuda=F,FULL=F,
   
   #respaldo para ver quienes no tiene pareja
   base_x["Originales_x"]=base_x["base_col"]
-  
+
   #se checa join
   if(por_join){
     base_output=as.list(NULL)
-    base_output[["en_comun"]]=base_x %>% inner_join(base_y ,by="base_col") %>% select(base_col)
-    base_output[["todo_en_x"]]=base_x %>% left_join(base_y ,by="base_col")
+    if(recursion){#se usa condicional porque en la recursividad no se quiere volver a calcular la base en comun
+      #de esta forma de ahorra tiempo de ejecución
+      base_output[["en_comun"]]=base_x %>% inner_join(base_y ,by="base_col") %>% select(base_col)
+      message("Excluido_y incluye los elementos que no estan en x pero si en y")
+    }
     base_output[["todo_en_y"]]=base_x %>% right_join(base_y ,by="base_col")
     base_output[["Excluidos_y"]]=base_x %>% right_join(base_y ,by="base_col") %>%filter(is.na(Originales_x))
-    message("Excluido_y incluye los elementos que no estan en x pero si en y")
   }else{
     message("Use all=T para obtener los elementos que no estan en x pero si en y")
     base_output=merge(base_x,base_y,by = "base_col",...)
   }
   
-  if(FULL){
-    base_y["Originales_x"]=base_y["base_col"]
+  if(FULL & por_join){#se usa por join y full ya que no se puede usar full sin join
     base_x["Originales_x"]=NULL
     
     #Resolver luego posible generador de latencia uwu reparar luego
-    base_output[["Excluidos_x"]]=tienen_lo_mismo(base_x = base_y,base_y = base_x,FULL=F,por_merge = F)$Excluidos_y
+    Salida=tienen_lo_mismo(base_x = base_y,base_y = base_x,por_merge = F,recursion = F)
+    base_output[["todo_en_x"]]=Salida$todo_en_y
+    base_output[["Excluidos_x"]]=Salida$Excluidos_y
     message("Excluido_x incluye los elementos que no estan en y pero si en x")
+    
+    #se cambia nombre de los elementos recursivos para que tenga sentido que falta
+    names(base_output[["todo_en_x"]])=c("base_col","Originales_y")
+    names(base_output[["Excluidos_x"]])=c("base_col","Originales_y")
   }
   
 
