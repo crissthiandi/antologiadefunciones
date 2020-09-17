bc6a71383fd2e4efd850b3bf9dcc4dec5e2c6539
crissthiandi <albertocenaa@gmail.com>
2020-09-16 22:45:08 -0500

    Alto, Se declara la función y como operara: Se ingresan las restricciones de los parametros

---
+++ nombres_lm.R
@@ -0,0 +1,19 @@
+#busca devolver el nombre de variables en un regresión lineal si se codfica como abajo
+
+# de esta forma se codifican los nombres
+
+#cambio de nombre para lm()
+variables=names(base_Modelar)
+variables_aux=names(base_Modelar)
+variables[3:25]=paste0("c",as.character(seq2(1,23)))
+names(base_Modelar)=variables
+remove(variables)
+
+
+nombres= function(modelo) {
+  stopifnot(is.list(modelo) | is.lm(modelo))
+  if(!exists("seleccion")){
+    message("Se debe llamar la base selección")#aquella que tiene los nombres con sus codigos
+  }
+
+}
