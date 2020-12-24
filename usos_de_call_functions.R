f172ba89d8d44e80518872f5a86ddef997f89b9a
crissthiandi <albertocenaa@gmail.com>
2020-12-24 13:11:24 -0600

    Uso de las funciones call: Estas son las funciones que nos ayudan a  hacer llamadas en un scope de algun tipo se intenta hacer uso de las formulas usadas en R

---
+++ usos_de_call_functions.R
@@ -0,0 +1,21 @@
+testeo <- function(formula,datos) {
+  ret.x <- x
+  ret.y <- y
+  cl <- match.call()
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data", "subset", "weights", "na.action",
+               "offset"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  if (method == "model.frame")
+    return(mf)
+  else if (method != "qr")
+    warning(gettextf("method = '%s' is not supported. Using 'qr'",
+                     method), domain = NA)
+  mt <- attr(mf, "terms")
+  y <- model.response(mf, "numeric")
+  w <- as.vector(model.weights(mf))
+}
+testeo(Sepal.Length ~ Petal.Length,iris)
