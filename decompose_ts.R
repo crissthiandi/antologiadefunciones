6aff86293afa863c58c77b055b8419044d68cf44
crissthiandi <albertocenaa@gmail.com>
2020-12-29 17:33:10 -0600

    Función auxiliar en descomposición de series de tiempo por un  metodo diferente al de promedios moviles

---
+++ decompose_ts.R
@@ -0,0 +1,25 @@
+#Codigo hecho y en mantenimiento por @crissthiandi <albertocenaa@gmail.com>
+#última versión en github.com/crissthiandi/antologiadefunciones
+
+
+#' Descomposición de series de tiempo by any smooth
+#'
+#' Descompone tu serie por estacionalidad, tendencia y ruido. Personaliza el suavizamiento a usar, por defecto \code{\link{loess}} smooth
+#'
+#' Similar a \code{\link{decompose}} el modelo aditivo es de la forma
+#' \t $Y[t] = T[t]+S[t]+e[t]$
+#'
+#' @param time_serie Un objeto series de tiempo
+#' @param type Tipo de estacionalidad.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+decompose_ts <- function(time_serie,type) {
+
+}
+
+
+
+
