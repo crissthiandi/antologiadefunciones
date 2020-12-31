a5b765e4573d5a559c511ba2e27a3d564ce7671f
crissthiandi <albertocenaa@gmail.com>
2020-12-31 14:28:45 -0600

    Se agrega logica para obtener una mejor propuesta: se usa TSA paqueteria para obtener los valores significativos de AR o MA Se mantiene la dependendicia a insertar los objetos acf

--- recomendacion_autocorrelacion.R
+++ recomendacion_autocorrelacion.R
@@ -30,26 +30,51 @@ recomendacion_autocorrelaciones <- function(objeto_cf,print_IC=FALSE) {
   a=as.character(a)
   if(length(a)!=3 | as.logical(a[3])){
     message("EL objeto debe ser un ACF o PACF con parametro plot = FALSE")
-    message("Ver el ejemplo de la documentación")
-    eval(?recomendacion_autocorrelaciones)
-
+    message("Ver el ejemplo en la documentación")
+    ?recomendacion_autocorrelaciones
   }
   #si la salida es un vector de 3 elementos entonces hay dos parametros
+
+  serie=get(objeto_cf$series)
+  if(objeto_cf$type=="partial"){
+    matriz=TSRutina::matriz_eacf(serie,ar.max = 1, ma.max = 15)
+    matriz=matriz$symbol=="o"
+    for(i in 1:15){
+      if(matriz[1,i]==1){
+        order_=i
+        cat("\nProponemos AR(p) con p=:",order_)
+        break
+      }
+    }
+
+  }
+  if(objeto_cf$type=="correlation"){
+    matriz=TSRutina::matriz_eacf(serie,ar.max = 15,ma.max = 1)
+    matriz=matriz$symbol=="o"
+    for(i in 1:15){
+      if(matriz[1,i]==1){
+        order_=i
+        cat("\nProponemos MA(q) con q=:",order_)
+        break
+      }
+    }
+  }
+
 
 
   #obtener los intervalos de confianza dando el objeto
   IC=intervalo_confianza_acf(objeto_cf)
-  mayores=abs(objeto_cf$acf)>IC
-  cat("\nLos siguientes elementos son propuestas de r: ")
-  posibles_lags=objeto_cf$lag[mayores]
-  cat(posibles_lags)
-  cat("\nProponemos que r sea:",posibles_lags[length(posibles_lags)])
+  #mayores=abs(objeto_cf$acf)>IC
+  #cat("\nLos siguientes elementos son propuestas de r: ")
+  #posibles_lags=objeto_cf$lag[mayores]
+  #cat(posibles_lags)
+  #cat("\nProponemos que r sea:",posibles_lags[length(posibles_lags)])
 
   if(print_IC){
     cat("\nEl IC de modelo es: ",IC)
   }
 
-  return(invisible(posibles_lags[length(posibles_lags)]))
+  return(invisible(order_))
 }
 
 
