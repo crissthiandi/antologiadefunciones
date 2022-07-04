df82b21d3f8477128783730cda2842e42538fc5e
Crissthiandi <albertocenaa@gmail.com>
2022-07-04 13:02:18 -0500

    Fix #10

---
+++ decorators.R
@@ -0,0 +1,22 @@
+#' Fix #10 ¿cuales son los intentos de tener decoradores en R?
+#' 
+#' visite https://www.r-bloggers.com/2020/03/how-to-create-decorators-in-r/
+#' la realiadad es que esta implementación de decoradores parece ser la solución, 
+#' tiene sugar syntax y todo. Pero tiene unos detalles.
+#' 
+#' Para empezar es una libreria totalmente ajena a la CRAN
+#' lo cual al principio no tiene nada de malo pero que llama la atención
+#' 
+#' segundo el repositorio de github donde se aloja no ha tenido updates desde el 2016...
+#' 
+#' Esto puede ser un problema, sin embargo es normal pensar que hay proyectos simples que no necesitan tantas mods...
+#' 
+#' tercero, tiene un rival muy convincente y que parece no abusar de la api de rstudio.
+#' 
+#' 
+#' Otros proyectos:
+#' https://github.com/klmr/decorator
+#' Este proyecto sigue vigente pero tiene un enorme cartel,
+#' al menos al dia 04/jul/2022 que avisa que la api es actualmente
+#' muy volatil, pues esta en desarrollo
+#' 
